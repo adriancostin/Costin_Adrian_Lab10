666bdf34eed62510c64ec00db8b2c725294ee0e6
Adrian Costin <adriancostin@users.noreply.github.com>
2021-12-10 18:41:53 +0200

    Lab11

---
+++ Costin_Adrian_Lab10/Models/ListProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Costin_Adrian_Lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+
+        public ListProduct()
+        {
+        }
+    }
+}
---
+++ Costin_Adrian_Lab10/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System.Collections.Generic;
+
+namespace Costin_Adrian_Lab10.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+
+        public Product()
+        {
+        }
+    }
+}
