771f84c0ebac0d821a423e59fa5a960c5679af7c
Adrian Costin <adriancostin@users.noreply.github.com>
2021-12-10 17:51:00 +0200

    Lab10

---
+++ Costin_Adrian_Lab10/ListPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Costin_Adrian_Lab10.Models;
+using Xamarin.Forms;
+
+namespace Costin_Adrian_Lab10
+{
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+    }
+}
---
+++ Costin_Adrian_Lab10/Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace Costin_Adrian_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public ShopList()
+        {
+           
+        }
+    }
+}
